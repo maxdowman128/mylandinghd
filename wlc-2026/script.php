680dd91cb66597a3b9f6c8e610d42169fc137f09
maxdowman128 <monkeydlufi128@gmail.com>
2026-07-07 23:01:06 +0600

    Add files via upload

---
+++ wlc-2026/script.php
@@ -0,0 +1,30 @@
+<?php
+
+function getTitle() {
+    $defaultTitle = 'FIFA World Cup 26™';
+    if (isset($_GET['m'])) {
+        return htmlspecialchars($_GET['m']);
+    } else {
+        return $defaultTitle; 
+    }
+}
+
+function getImage() {
+    $defaultImage = 'picture/default_image.jpg';
+    if (isset($_GET['p'])) {
+        $imageName = strtolower(htmlspecialchars($_GET['p'])) . '.jpg';
+        $imagePath = 'picture/' . $imageName;
+        
+        if (file_exists($imagePath)) {
+            return $imagePath;
+        } else {
+            return $defaultImage; 
+        }
+    } else {
+        return $defaultImage; 
+    }
+}
+
+$title = getTitle();
+$image = getImage();
+?>
